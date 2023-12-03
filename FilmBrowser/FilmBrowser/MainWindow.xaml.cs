a5ab3481a7044d1f7b7a9b462019451a6b359bb1
JoshMacKayNSCC <w0268352@campus.nscc.ca>
2023-12-03 13:18:12 -0400

    Created the project with a basic navigation toolbar and menu.

---
+++ FilmBrowser/FilmBrowser/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilmBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Page homePage = new Pages.Home();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Frame_PageDisplay.NavigationService.Navigate(homePage);
+        }
+
+        private void Navigate_Home(object sender, RoutedEventArgs e)
+        {
+            Frame_PageDisplay.NavigationService.Navigate(homePage);
+        }
+
+        private void Navigate_Directors(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Navigate_Actors(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Navigate_Movie_Catalog(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
